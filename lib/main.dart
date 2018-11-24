e8ad52788e86efcb5a6adb397d57a59648f0a02c
Draftbk <draftbks@gmail.com>
2018-11-24 11:25:50 -0500

    try to build a chat room

--- lib/main.dart
+++ lib/main.dart
@@ -27,15 +27,39 @@ class ChatScreen extends StatefulWidget {                     //modified
 
 // Add the ChatScreenState class definition in main.dart.
 
-class ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];             // new
   final TextEditingController _textController = new TextEditingController(); //new//new
   @override                                                        //new
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat")),
-      body: _buildTextComposer(),
+      body: new Column(                                        //modified
+        children: <Widget>[                                         //new
+          new Flexible(                                             //new
+            child: new ListView.builder(                            //new
+              padding: new EdgeInsets.all(8.0),                     //new
+              reverse: true,                                        //new
+              itemBuilder: (_, int index) => _messages[index],      //new
+              itemCount: _messages.length,                          //new
+            ),                                                      //new
+          ),                                                        //new
+          new Divider(height: 1.0),                                 //new
+          new Container(                                            //new
+            decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor),                  //new
+            child: _buildTextComposer(),                       //modified
+          ),                                                        //new
+        ],                                                          //new
+      ),
     );
   }
+  @override
+  void dispose() {                                                   //new
+    for (ChatMessage message in _messages)                           //new
+      message.animationController.dispose();                         //new
+    super.dispose();                                                 //new
+  }                                                                  //new
   Widget _buildTextComposer(){
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
@@ -60,10 +84,57 @@ class ChatScreenState extends State<ChatScreen> {
       )
     );
   }
+
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(                         //new
+      text: text,                                                  //new
+      animationController: new AnimationController(
+          duration: new Duration(milliseconds: 700),
+          vsync: this,
+      ),
+    );                                                             //new
+    setState(() {                                                  //new
+      _messages.insert(0, message);                                //new
+    });
+    message.animationController.forward();
   }
 }
 
-
-
+// Add the following class definition to main.dart.
+const String _name = '小爪';
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(                                    //new
+        sizeFactor: new CurvedAnimation(                              //new
+            parent: animationController, curve: Curves.easeOut),      //new
+        axisAlignment: 0.0,                                           //new
+        child: new Container(                                    //modified
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new CircleAvatar(child: new Text(_name[0])),
+              ),
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )                                                           //new
+    );
+  }
+}
